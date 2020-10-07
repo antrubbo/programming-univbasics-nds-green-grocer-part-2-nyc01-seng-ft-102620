47a2969fe05b07cfe4c301caed013bdb72d74cc8
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-07 18:26:11 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -48,6 +48,6 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-  consolidate_cart(cart)
+  new_cart = consolidate_cart(cart)
   
 end
